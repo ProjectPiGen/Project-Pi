e21057a32d6f736e4525debad53c0c9856c0e8c0
Donnie Brown <donnie@Brown.local>
2013-12-13 14:00:11 -0500

    OK03

--- template/source/main.s
+++ template/source/main.s
@@ -2,35 +2,59 @@
 .globl _start
 _start:
 
-ldr r0,=0x20200000
+b main
 
-mov r1,#1
-lsl r1,#18
-str r1,[r0,#4]
+.section .text
+main:
+mov sp,#0x8000
 
-mov r1,#1
-lsl r1,#16
+/*SetGpioFunction*/
+pinNum .req r0
+pinFunc .req r1
+mov pinNum,#16
+mov pinFunc,#1
+bl SetGpioFunction
+.unreq pinNum
+.unreq pinFunc
 
+/*Blink loop*/
 loop$:
-str r1,[r0,#40]
 
-/*
-*loop$:
-*/b loop$
+/*Turn on OK LED*/
+pinNum .req r0
+pinVal .req r1
+mov pinNum,#16
+mov pinVal,#0
+bl SetGpio
+.unreq pinNum
+.unreq pinVal
 
-mov r2,#0x3F0000
+/*Create Wait(Delay) to keep LED on*/
+decr .req r0
+mov decr,#0x3F0000
 wait1$:
-	sub r2,#1
-	cmp r2,#0
+	sub decr,#1
+	teq decr,#0
 	bne wait1$
+.unreq decr
 
-str r1,[r0,#28]
+/*Turn off OK LED*/
+pinNum .req r0
+pinVal .req r1
+mov pinNum,#16
+mov pinVal,#1
+bl SetGpio
+.unreq pinNum
+.unreq pinVal
 
-mov r2,#0x3F0000
+/*Create Wait(Delay) to keep LED off*/
+decr .req r0
+mov decr,#0x3F0000
 wait2$:
-	sub r2,#1
-	cmp r2,#0
+	sub decr,#1
+	teq decr,#0
 	bne wait2$
+.unreq decr
 
-
+/*Loop LED blink*/
 b loop$
